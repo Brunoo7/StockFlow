7f2776b72d357391eacffad9fd907de090423fea
Brunoo7 <brunoolivera072@gmail.com>
2026-08-18 20:22:08 -0300

    adiciona funções para calcular quantidade de produtos cadastrados, sem estoque, produto com maior estoque, produto mais vendido e faturamento total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from produtos import cadastro_produtos, lista_produtos, busca_produto, atualizar_estoque
-from vendas import registrar_venda, listar_vendas, mostrar_faturamento
+from produtos import cadastro_produtos, lista_produtos, busca_produto, atualizar_estoque, produtos_cadastrados
+from vendas import registrar_venda, listar_vendas, mostrar_faturamento, regist_vendas
 import os 
 
 def voltar_menu():
@@ -9,9 +9,66 @@ def voltar_menu():
 def sair():
     print("Fechando programa...")
     os.system("cls")
-    menu()
+
+def quantidade_registrados():
+    quantidade_produtos = 0
+    for produto in produtos_cadastrados:
+        quantidade_produtos += 1
+    print(f"Quantidade de produtos cadastrados {quantidade_produtos}")
+
+def quantidade_sem_estoque():
+    sem_estoque = 0
+    for produto in produtos_cadastrados:
+        estoque = produto["estoque"]
+    
+        if estoque == 0:
+            sem_estoque += 1
+    print(f"Quantidade de produtos sem estoque {sem_estoque}")
+
+def produto_maior_estoque():
+    maior_estoque = 0
+    produto_estoque = ""
+    for produto in produtos_cadastrados:
+        estoque = produto["estoque"]
+
+        if estoque > maior_estoque:
+            produto_estoque = produto["nome"]
+            maior_estoque = produto["estoque"]
+    print(f"O produto {produto_estoque} tem o maior estoque com {maior_estoque} itens")
+
+def produto_mais_vendido():
+    maior_venda = 0
+    produto_maior_venda = ""
+    venda_por_produto = {}
+    for venda in regist_vendas:
+        nome = venda["nome"]
+        ven = venda["número de itens vendidos"]
+        if nome not in venda_por_produto:
+            venda_por_produto[nome] = ven
+        else:
+            venda_por_produto[nome] += ven
+    for venda in venda_por_produto:
+        if maior_venda < venda_por_produto[venda]:
+            produto_maior_venda = venda
+            maior_venda = venda_por_produto[venda]
+
+    print(f"Produto {produto_maior_venda} é o mais vendido com {maior_venda} vendas")
+
+def fatur_total():
+    faturamento_total = 0
+    for produto in regist_vendas:
+        vlr_total = produto["valor total da venda"]
+        faturamento_total = faturamento_total + vlr_total
+
+    print(f"Faturamento total {faturamento_total}")
 
 def mostrar_relatorio():
+    quantidade_registrados()
+    quantidade_sem_estoque()
+    produto_maior_estoque()
+    produto_mais_vendido()
+    fatur_total()
+    menu()
 
 def opcoes():
     print("1- Cadastro de produtos")
@@ -52,3 +109,4 @@ def menu():
                 sair()
     except:
         print("Opção inválida, tente novamente")
+menu()
