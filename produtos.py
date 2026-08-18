3e84401f5089e8bcfee71cc2baca6cbbe9df3482
Brunoo7 <brunoolivera072@gmail.com>
2026-08-18 14:25:25 -0300

    adiciona função para listar produtos cadastrados

--- produtos.py
+++ produtos.py
@@ -11,3 +11,13 @@ def cadastro_produtos():
     novo_produto = {"nome": nome, "categoria": categoria, "preço": preco, "estoque": estoque}
     produtos_cadastrados.append(novo_produto)
     print(f"O produto {nome} foi cadastrado com sucesso!")
+
+def lista_produtos():
+    for produto in produtos_cadastrados:
+        print("Lista de produtos")
+        print(f"{"Nome do produto".ljust(20)} | {"Categoria".ljust(20)} | {"Preço".ljust(20)} | {"Estoque"}")
+        nome = produtos_cadastrados["nome"]
+        categoria = produtos_cadastrados["categoria"]
+        preco = produtos_cadastrados["preço"]
+        estoque = produtos_cadastrados["estoque"]
+        print(f"{nome.ljust(20)} | {categoria.ljust(20)} | {preco.ljust(20)} | {estoque}")
